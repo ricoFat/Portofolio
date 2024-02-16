5333fe7dc50a419bca99af0f2ab0826039f5fe73
ricoFat <accram2005@hotmail.com>
2024-02-16 02:32:17 -0500

    commit du portofolio

---
+++ assets/js/main.js
@@ -0,0 +1,27 @@
+var tabLinks = document.getElementsByClassName("tab-links");
+var tabContents = document.getElementsByClassName("tab-contents");
+var sidemenu = document.getElementById("sidemenu")
+
+function openTab(tabname) 
+{
+    for(let tabLink of tabLinks)
+    {
+        tabLink.classList.remove("active-link");
+    }
+    for(let tabContent of tabContents)
+    {
+        tabContent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+}
+
+function openmenu()
+{
+    sidemenu.style.right = "0";
+}
+
+function closemenu()
+{
+    sidemenu.style.right = "-200px";
+}
