7d8e4a0da3c916069ad51dc93bafde271e6183c1
ricoFat <accram2005@hotmail.com>
2024-02-23 01:25:47 -0500

    changement sur le responsive

--- assets/js/main.js
+++ assets/js/main.js
@@ -35,3 +35,18 @@ function closemenu()
 {
     sidemenu.style.right = "-200px";
 }
+
+function closeMenuOnClick() {
+    // Sélectionnez tous les liens dans votre menu latéral
+    var links = document.querySelectorAll("#sidemenu a");
+
+    // Parcourez tous les liens pour ajouter un gestionnaire d'événements de clic
+    links.forEach(function(link) {
+        link.addEventListener("click", function() {
+            // Appel de la fonction closemenu pour fermer le menu latéral
+            closemenu();
+        });
+    });
+}
+// Appel de la fonction closeMenuOnClick pour qu'elle soit exécutée au chargement de la page
+window.onload = closeMenuOnClick;
